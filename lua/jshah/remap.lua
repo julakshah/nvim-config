bf59a7d543f6a5152da97707e67c12f96278a8ab
Julian Shah <julakshah@gmail.com>
2025-09-11 18:54:13 -0400

    adapted base config for linux

--- lua/jshah/remap.lua
+++ lua/jshah/remap.lua
@@ -1,16 +1,16 @@
 vim.g.mapleader = " "
 -- General
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>w", vim.cmd.w)
-vim.keymap.set("n", "<leader>q", vim.cmd.q)
+vim.keymap.set("n", "<leader>pv", ":Ex<CR>")
+vim.keymap.set("n", "<leader>w", "w")
+vim.keymap.set("n", "<leader>q", "q")
 -- Window splitting
-vim.keymap.set("n", "<leader>o", vim.cmd.vsplit)
-vim.keymap.set("n", "<leader>p", vim.cmd.split)
+vim.keymap.set("n", "<leader>o", "vsplit")
+vim.keymap.set("n", "<leader>o", "split")
 -- Window navigation
 vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 -- Line movement
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+-- vim.keymap.set("v", "J", ":m '<+1<CR>gv=gv", { desc - "Move selection down" })
+-- vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc - "Move selection up" })
